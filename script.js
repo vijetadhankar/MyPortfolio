a338143513524be1f1b8a6b76c5e6a648eef0d28
vijetadhankar <93464584+vijetadhankar@users.noreply.github.com>
2024-02-27 01:57:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+
+function flipCard() {
+    var cardInner = document.getElementById('card-inner');
+    cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+}
