f8b0a52407d9ae0732cf1c47193a3f31897806db
vijetadhankar <93464584+vijetadhankar@users.noreply.github.com>
2024-02-29 18:18:40 +0530

    Responsive
    Made the portfolio responsive

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
-function flipCard() {
-    var cardInner = document.getElementById('card-inner');
+// function flipCard() {
+//     var cardInner = document.getElementById('card-inner');
+//     cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
+// }
+function flipCard(cardId) {
+    var cardInner = document.getElementById('card-inner' + cardId);
+
     cardInner.style.transform = cardInner.style.transform === 'rotateY(180deg)' ? 'rotateY(0deg)' : 'rotateY(180deg)';
 }
